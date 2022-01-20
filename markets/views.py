5196b8c59a73a76a0b4918c2e6d4bc33912f67fe
Spannakin <spannakin@gmail.com>
2022-01-20 14:20:02 +0000

    ADDED views and urls for markets

--- markets/views.py
+++ markets/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def markets(request):
+    """ A view to return the upcoming markets page """
+
+    return render(request, 'markets/markets.html')
