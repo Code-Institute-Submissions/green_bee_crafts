96fea416cca137f19ebbb7e69e72b8340deca25b
Spannakin <spannakin@gmail.com>
2022-01-27 14:59:23 +0000

    ADD REDAME colours added and css updated

--- bag/views.py
+++ bag/views.py
@@ -21,9 +21,7 @@ def add_to_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    
     bag = request.session.get('bag', {})
-
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
@@ -43,36 +41,18 @@ def adjust_bag(request, item_id):
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if size:
-        if quantity > 0:
-            bag[item_id]['items_by_size'][size] = quantity
-            messages.success(request,
-                             (f'Updated size {size.upper()} '
-                              f'{product.name} quantity to '
-                              f'{bag[item_id]["items_by_size"][size]}'))
-        else:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-            messages.success(request,
-                             (f'Removed size {size.upper()} '
-                              f'{product.name} from your bag'))
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.success(request,
+                            (f'Updated {product.name} '
+                            f'quantity to {bag[item_id]}'))
     else:
-        if quantity > 0:
-            bag[item_id] = quantity
-            messages.success(request,
-                             (f'Updated {product.name} '
-                              f'quantity to {bag[item_id]}'))
-        else:
-            bag.pop(item_id)
-            messages.success(request,
-                             (f'Removed {product.name} '
-                              f'from your bag'))
+        bag.pop(item_id)
+        messages.success(request,
+                            (f'Removed {product.name} '
+                            f'from your bag'))
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -83,21 +63,10 @@ def remove_from_bag(request, item_id):
 
     try:
         product = get_object_or_404(Product, pk=item_id)
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
         bag = request.session.get('bag', {})
 
-        if size:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-            messages.success(request,
-                             (f'Removed size {size.upper()} '
-                              f'{product.name} from your bag'))
-        else:
-            bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
