31f6035ad2cda7516438e39110f887ab5f8eb100
Spannakin <spannakin@gmail.com>
2022-02-02 10:58:35 +0000

    REMOVED secret key and SET debug

--- green_bee_crafts/settings.py
+++ green_bee_crafts/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sp$*o&$#==t3@=og2cy526n7mt@f7#aa1#np4ae(-m43y^8h#7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['green-bee-crafts.herokuapp.com', 'localhost']
 
