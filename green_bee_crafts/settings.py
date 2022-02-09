b98c2c8c7555bd3041dd12f0e5aa302d8d7b762c
Spannakin <spannakin@gmail.com>
2022-02-09 11:01:26 +0000

    ADDED full checkout functionality, urls, views and tested to see that it works

--- green_bee_crafts/settings.py
+++ green_bee_crafts/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     
 
     # other
-    'storages'
+    'storages',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'green_bee_crafts.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -80,8 +83,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'bag.contexts.bag_contents'
+                'django.template.context_processors.media',
+                'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
