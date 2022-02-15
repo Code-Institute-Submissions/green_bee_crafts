a8e6d8251e90e7f6d94da5e3bdcc6f82e69c816b
Spannakin <spannakin@gmail.com>
2022-02-15 15:24:55 +0000

    EDITED markets app not working fully

--- markets/views.py
+++ markets/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Market
 
 
 # Create your views here.
 def markets(request):
     """ A view to return all markets """
+    markets = Market.objects.all()    
 
-    return render(request, 'markets/markets.html')
+    context = {
+        'markets': markets
+    }
+
+    return render(request, 'markets/markets.html', context)
--- upcycle/views.py
+++ upcycle/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Upcycle
 
 
 # Create your views here.
 def upcycle(request):
-    """ A view to return the index page """
+    """ A view to return all upcycling information """
+    upcycle = Upcycle.objects.all()    
 
-    return render(request, 'upcycle/upcycle.html')
+    context = {
+        'upcycle': upcycle
+    }
+
+    return render(request, 'upcycle/upcycle.html', context)
